76f22c6d1b7835f6c111e488062220a02738597e
Brian Ford <btford@umich.edu>
2014-03-02 16:10:15 -0800

    update to angular@1.2.14 and angular-socket-io@0.3.0

--- public/js/app.js
+++ public/js/app.js
@@ -3,6 +3,8 @@
 // Declare app level module which depends on filters, and services
 
 angular.module('myApp', [
+  'ngRoute',
+
   'myApp.controllers',
   'myApp.filters',
   'myApp.services',
--- public/js/services.js
+++ public/js/services.js
@@ -6,4 +6,7 @@
 // Demonstrate how to register services
 // In this case it is a simple value service.
 angular.module('myApp.services', []).
+  factory('socket', function (socketFactory) {
+    return socketFactory();
+  }).
   value('version', '0.1');
